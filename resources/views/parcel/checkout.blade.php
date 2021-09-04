541cc333bd6153d8b2a8dcca14cd24c7a4c00421
stefanHSS <978544@Swansea.ac.uk>
2021-09-04 16:12:11 +0100

    Added minor styling to checkout page

--- resources/views/parcel/checkout.blade.php
+++ resources/views/parcel/checkout.blade.php
@@ -12,19 +12,46 @@
             @csrf
             <input type="hidden" name="parcel" id="parcel" value="{{$parcel}}">
             <input type="hidden" name="payment_method" class="payment-method" value="">
-            <input id="card-holder-name" type="text">
+            <input class="StripeElement mb-3" id="card-holder-name" type="text" placeholder="Card holder name" required>
 
             <!-- Stripe Elements Placeholder -->
             <div id="card-element"></div>
 
-            <button id="card-button">
-                Process Payment
-            </button>
+            <div class="form-group mt-3">
+                <button id="card-button" class="btn btn-primary">
+                    Process Payment
+                </button>
+            </div>
         </form>
     </div>
 
     @endsection
 </x-admin-master>
+
+@section('styles')
+<style>
+    .StripeElement {
+        box-sizing: border-box;
+        height: 40px;
+        padding: 10px 12px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        background-color: white;
+        box-shadow: 0 1px 3px 0 #e6ebf1;
+        -webkit-transition: box-shadow 150ms ease;
+        transition: box-shadow 150ms ease;
+    }
+    .StripeElement--focus {
+        box-shadow: 0 1px 3px 0 #cfd7df;
+    }
+    .StripeElement--invalid {
+        border-color: #fa755a;
+    }
+    .StripeElement--webkit-autofill {
+        background-color: #fefde5 !important;
+    }
+</style>
+@endsection
 
 @section('scripts')
 <script src="https://js.stripe.com/v3/"></script>
